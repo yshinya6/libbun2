19669e584fac5076636f678040ddff05d6ff3fff
Yuji Tamura <yuji.tamura.kt@gmail.com>
2014-06-18 18:17:21 +0900

    squash 15 commits of js into 1 commit

---
+++ test-peg/javascript/Operate.js
@@ -0,0 +1,41 @@
+﻿a = 10;
+b = 2;
+c = "10";
+d = true;
+e = bool;
+
+a + b;
+a - b;
+a * b;
+a / b;
+a % b;
+
+a++;
+++b;
+a--;
+--b;
+
+a == b;
+a != b;
+a < b;
+a <= b;
+a > b;
+a >= b;
+
+a === c;
+a !== c;
+
+d && e;
+d || e;
+!e;
+
+d ? "hoge" : "hogehoge";
+
+a & b;
+a | b;
+a ^ b;
+~a;
+
+a << 2;
+a >> 2;
+a >>> 2;
